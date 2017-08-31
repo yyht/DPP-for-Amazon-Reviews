5dc1d4a0b64c291bb724f16a1e278ba9680a4235
mahrab <barham@knights.ucf.edu>
2017-08-31 17:17:12 -0400

    Add files via upload

--- Quality.py
+++ Quality.py
@@ -1,38 +1,46 @@
 import json
 import scipy.io
 import numpy
-from sumy.parsers.plaintext import PlaintextParser
+from sumy._compat import to_unicode
+from sumy.models.dom import Sentence, Paragraph, ObjectDocumentModel
 from sumy.nlp.tokenizers import Tokenizer 
-import Importance
+import lex_rank_modified
 
 def calc_quality():
 
     with open("reviews_by_seed.json") as in_file:
         data = json.load(in_file)
     
-    sep = " "
-    docs = []
-    num_docs = 0
-    for kk in data:
-        for sentence in range(len(data[kk])):
-            data[kk][sentence] = data[kk][sentence].replace('.',' ')+'.'
+    #TODO: Make use of stopword list
+    with open("stopwords.txt") as s_file:
+        stopword_list = s_file.read().split()
 
-        docs.append(sep.join(data[kk]))
-        num_docs += 1
-    
-    summarizer = Importance.LexRankSummarizer()
-    theta = numpy.ones(1)
+    k = 0
+    p = []
     md = {}
-    for k in xrange(num_docs):
-        parser = PlaintextParser.from_string(docs[k], Tokenizer("english"))
-        ratings = summarizer(parser.document)
+    summarizer = lex_rank_modified.LexRankSummarizer()
+    #summarizer.stop_words(stopword_list)
+
+    for kk in data:
+        prelim_doc = list(map(preprocess,data[kk]))
+        s = list(map(to_sentence, prelim_doc))
+        p.append(Paragraph(s))
+        d = ObjectDocumentModel(p)
+        ratings = summarizer(d)
         feat_matrix = numpy.matrix(ratings).transpose()
-        qual_matrix = numpy.exp(feat_matrix*theta)
+        qual_matrix = numpy.exp(feat_matrix)
         md['q' + str(k+1)] = qual_matrix
+        k+=1
+        p = []
 
     scipy.io.savemat('qual.mat', md)
     return
 
+def preprocess(text):
+    return to_unicode(text).strip()
+
+def to_sentence(text):
+    return Sentence(text, Tokenizer("english"))
 
 if __name__ == '__main__':
     calc_quality()
