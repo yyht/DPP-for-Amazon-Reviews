55b51b105b37119bcacc19579cbdd80cddb23525
mahrab <barham@knights.ucf.edu>
2017-09-04 17:16:06 -0400

    Add files via upload

--- proto.m
+++ proto.m
@@ -1,46 +1,52 @@
-% given a random quality and similarity matrix, select the k best documents
-m = 10; % the number of hypothetical documents
-k = 5; % the number we'll be selecting
-q = rand(m,1); % the dummy quality matrix, m quality vectors of length 5
-S = rand(m); % the dummy similarity matrix
+% given a set of quality and similarity matrices, select the k best
+% sentences from each document
 
-for i = 1 : m
-    S(i,i) = 0;
-end
+n_selections = 10;
 
-b = 665; % the standard by DUC
+qual = load('qual.mat');
+qual = orderfields(qual);
+qual_fields = fieldnames(qual);
 
-L = S .* (q*q'); %Lij = qi*qj*Sij       
-L = 0.5*(L+L');
-[V, Lam] = eig(full(L));
-Lam(Lam<0) = 0;
-L = V*Lam*V';    % project L into the PSD cone
+sim = load('sim.mat');
+sim = orderfields(sim);
+sim_fields = fieldnames(sim);
 
-flag = true(m,1); % initialize all elements to be valid candidates
-%flag(sum(doc.txt>0,2) < min_words) = false; % There is no need to filter
-%by sentence lenth for now
+Selections = [];
 
+%function Curr_Selections = calc_DPP(qual_mat,sim_mat)
+for curr_doc = 1 : length(qual_fields)
+    qual_mat = qual.(char(qual_fields(curr_doc)));
+    sim_mat = sim.(char(sim_fields(curr_doc)));
+    
+    L = sim_mat .* (qual_mat*qual_mat');       
+    L = 0.5*(L+L');
+    [V, Lam] = eig(full(L));
+    Lam(Lam<0) = 0;
+    L = V*Lam*V';
 
-Y = []; % initialize y to be empty
-val_old = 0; % initialize the old value of det(Ly) to 0
-%while any(flag) % so long as any valid candidates remain
-for t = 1 : k
-    inds = find(flag); % create a vector containing their indices
-    p = zeros(size(inds)); % and initialize a vector of zeroes of the same length
-    for iter = 1 : length(inds) % for each valid candidate
-        i = inds(iter); % take its index
-        Ytmp = [i; Y]; % and add it to the top of Ytmp
-        p(iter) = (det(L(Ytmp,Ytmp)) - val_old); % then the corresponding value of p to det(Lytmp) - det(Ly)
+    candidates = true(length(qual_mat),1);
+
+    Curr_Selections = [];
+    val_old = 0;
+
+    for t = 1 : n_selections
+        inds = find(candidates);
+        p = zeros(size(inds));
+        
+        for iter = 1 : length(inds)
+            i = inds(iter);
+            Curr_Selections_tmp = [i; Curr_Selections];
+            p(iter) = (det(L(Curr_Selections_tmp,Curr_Selections_tmp)) - val_old);
+        end
+        
+        [val, pos] = max(p);
+        Curr_Selections = [inds(pos); Curr_Selections];
+        val_old = det(L(Curr_Selections,Curr_Selections));
+        candidates(Curr_Selections) = false;
     end
-    [val, pos] = max(p); % take the max of p
-     %if val < val_old
-        %break;
-     %end
-    Y = [inds(pos); Y]; % and add its corresponding index of flags to Y
-    val_old = det(L(Y,Y)); % update det(Ly)
-    %lenY = sum(doc.cost(Y));
-    flag(Y) = false; % remove the candidate just selected from the list of candidates
-    %flag(doc.cost > b - lenY) = false;
+    
+    Selections = horzcat(Curr_Selections, Selections);
 end
 
- Y=flip(Y); %reverse the order of Y, so the first candidate selected is on top
+Selections = Selections';
+save('sel.mat','Selections');
